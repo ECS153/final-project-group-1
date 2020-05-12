15a68a1adb95946776ee3dae29331f2a7e20f2ad
SunGoesRowr <heplin@ucdavis.edu>
2020-05-11 21:48:34 -0700

    Completed hash generator function
    The function returns a nonce that can be used to generate a hash that starts with a certain amount of zeros.

--- server/hash_generator.py
+++ server/hash_generator.py
@@ -1,11 +1,13 @@
 from hashlib import sha256
+from random import randrange
+
 
 def generate_hash(prev_block_hash, merkle_root, num_starting_0s):
     """
     Creates a nonce for the block that is to be published to the blockchain.
 
-    To make block chain secure, generating hashes needs to be difficult. This 
-    function will hash the string prev_block_hash+merkle_root+nonce 
+    To make block chain secure, generating hashes needs to be difficult. This
+    function will hash the string prev_block_hash+merkle_root+nonce
     until the hash produced using SHA-256 starts with num_starting_0s. The nonce is simply
     a number appended to the end of the string.
 
@@ -18,3 +20,19 @@ def generate_hash(prev_block_hash, merkle_root, num_starting_0s):
     The nonce such that SHA156.Hash(prev_block_hash+merle_root+nonce).Hex()
     starts with num_starting_0s 0s.
     """
+    nonce = str(randrange(0, 300001))
+    hash_string = prev_block_hash + merkle_root + nonce
+    my_hash = sha256(hash_string.encode('utf-8')).hexdigest()
+    num_zeros = make_zeros_str(num_starting_0s)
+
+    if my_hash.startswith(num_zeros):
+        return nonce
+    else:
+        return generate_hash(prev_block_hash, merkle_root, num_starting_0s)
+
+
+def make_zeros_str(num_zero):
+    value = ""
+    for x in range(num_zero):
+        value = value + "0"
+    return value
