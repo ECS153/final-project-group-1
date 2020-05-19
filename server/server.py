9c2d7073346564ed52fc74cea0c07879c90e3acd
DarianAbdullah <43215562+DarianAbdullah@users.noreply.github.com>
2020-05-19 11:04:06 -0700

    Updated do_PUT which puts messages into the database
    I used a dictionary approach instead of f-String to prevent sql injection.

--- server/server.py
+++ server/server.py
@@ -1,5 +1,6 @@
 from http.server import HTTPServer
 from http.server import BaseHTTPRequestHandler
+import sqlite3
 
 PORT = 5001
 
@@ -13,13 +14,27 @@ class BlockchainClientHandler(BaseHTTPRequestHandler):
         r.wfile.write(b"No messages yet.")
 
     def do_PUT(r):
-        print("Sender: "+r.headers["sender"])
-        print("Receiver: "+r.headers["receiver"])
+        sender = r.headers["sender"]
+        receiver = r.headers["receiver"]
+        message = str(r.rfile.read(int(r.headers["content-length"])))
+        print("Sender: "+sender)
+        print("Receiver: "+receiver)
         print("Password: "+r.headers["password"])
-        print("Message: "+str(r.rfile.read(int(r.headers["content-length"]))))
+        print("Message: "+message)
         r.send_response(200)
         r.end_headers()
         r.wfile.write(b"Received all the headers!")
+
+        conn = sqlite3.connect('message.db')
+        c = conn.cursor()
+        with conn:
+            c.execute("INSERT INTO messages VALUES \
+            (:sender, :receiver, :message, :isSent)",
+            {'sender': sender, 'receiver': receiver,
+            'message': message, 'isSent': 0})
+        print("Printing what was inputting in the db: "+c.fetchall())
+        conn.commit()
+        conn.close()
 
 def main():
     """
