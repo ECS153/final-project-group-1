347f68982a5cd34b3dea620f4548b94749e010af
Zachary Anway <zanway578@gmail.com>
2020-05-28 11:42:53 -0700

    Merklechain in code docs

--- server/merklechain.py
+++ server/merklechain.py
@@ -2,6 +2,9 @@ from merkletree import MerkleTree
 from hashlib import sha256
 
 class _node:
+    """
+    Node class that contains tree, prev_hash of block, nonce
+    """
     def __init__(self, messages, prev_hash, nonce):
         self.tree = MerkleTree(messages)
         self.prev_hash = prev_hash
@@ -10,21 +13,34 @@ class _node:
 
 class MerkleChain:
     def __init__(self):
-        ## construct root node using a default set of messages and precomputed nonce
+        """
+        Adds first default block.
+        """
         default_msges = ["abgh", "1", "asd", "153", "153", "190", "100", "190"]
         self.first = _node(default_msges, "", 1000)
         self.last = self.first
         self.latest_block_idx = 0
     
     def insert(self, messages, nonce):
-        ## TODO: Get hash of latest block
-        self.last.nxt = _node(messages, "", nonce)
+        """
+        Inserts message group into chain.
+
+        Return:
+        The index of the block the messages are placed into.
+        """
+        prev_hash = self.get_latest_block_hash()
+        self.last.nxt = _node(messages, prev_hash, nonce)
         self.last = self.last.nxt
         self.latest_block_idx += 1
         return self.latest_block_idx
 
     def get_latest_block_hash():
-        return sha256(self.prev_hash + self.last.tree.getMerkleRoot() + str(self.nonce))
+        """
+        Constructs and returns SHA-256 hash of current leading node. 
+        """
+        return sha256(self.last.prev_hash + 
+                      self.last.tree.getMerkleRoot() + 
+                      str(self.last.nonce))
 
 #m = MerkleChain()
 #for i in range(0, 10):
